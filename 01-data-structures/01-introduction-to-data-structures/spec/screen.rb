38bb821dd9acf453dca54dfb1b187f3158d2e09b
Geoff Hull <geoffhull03@gmail.com>
2017-09-28 12:52:24 -0700

    Finished Assignment 1

---
+++ 01-data-structures/01-introduction-to-data-structures/spec/screen.rb
@@ -0,0 +1,37 @@
+require_relative 'pixel'
+
+class Screen
+  attr_accessor :width
+  attr_accessor :height
+  attr_accessor :matrix
+
+  def initialize(width, height)
+    self.width = width
+    self.height = height
+    self.matrix = Array.new(height) {Array.new(width)}
+  end
+
+  # Insert a Pixel at x, y
+  def insert(pixel, x, y)
+    self.matrix[x][y] = pixel
+  end
+
+  def at(x, y)
+    if inbounds(x, y)
+      self.matrix[x][y]
+    else
+      nil
+    end
+  end
+
+  private
+
+  def inbounds(x, y)
+    if x > 0 and x < self.width and y > 0 and y < self.height
+      true
+    else
+      false
+    end
+  end
+
+end
